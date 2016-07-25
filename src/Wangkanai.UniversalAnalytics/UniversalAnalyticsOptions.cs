aae9de7c6df839ab98b7d605045db078437dc9da
sarin <sarin@wangkanai.comc>
2016-07-25 16:27:32 +0700

    commit sample project usage

--- src/Wangkanai.UniversalAnalytics/UniversalAnalyticsOptions.cs
+++ src/Wangkanai.UniversalAnalytics/UniversalAnalyticsOptions.cs
@@ -1,6 +1,30 @@
-﻿namespace Wangkanai.UniversalAnalytics
+﻿using System;
+using System.Collections.Generic;
+using Wangkanai.UniversalAnalytics.Infrastructure;
+using Wangkanai.UniversalAnalytics.Utilities;
+
+namespace Wangkanai.UniversalAnalytics
 {
-    public abstract class UniversalAnalyticsOptions
+    public abstract class UniversalAnalyticsOptions : IUniversalAnalyticsOptions
     {
+        private readonly IReadOnlyDictionary<Type, IUniversalAnalyticsOptionsExtensions> _extensions;
+
+        protected UniversalAnalyticsOptions(
+            IReadOnlyDictionary<Type, IUniversalAnalyticsOptionsExtensions> extensions)
+        {
+            Check.NotNull(extensions, nameof(extensions));
+
+            _extensions = extensions;
+        }
+
+        public virtual IEnumerable<IUniversalAnalyticsOptionsExtensions> Extensions 
+            => _extensions.Values;
+
+        public virtual T FindExtension<T>() 
+            where T : class, IUniversalAnalyticsOptionsExtensions
+        {
+            IUniversalAnalyticsOptionsExtensions extensions;
+            return _extensions.TryGetValue(typeof(T), out extensions) ? (T) extensions : null;
+        }
     }
 }
