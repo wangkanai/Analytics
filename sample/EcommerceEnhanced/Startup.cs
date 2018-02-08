f03f0d08b856009b80f2b45fdfe33a02e7812e0d
Sarin Na Wangkanai <sarin@wangkanai.com>
2018-02-08 21:30:09 +0700

    update demo projects

--- sample/EcommerceEnhanced/Startup.cs
+++ sample/EcommerceEnhanced/Startup.cs
@@ -18,7 +18,7 @@ namespace AnalyticsSandbox
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {            
+        {
             // Add framework services.
             services.AddMvc();
 
