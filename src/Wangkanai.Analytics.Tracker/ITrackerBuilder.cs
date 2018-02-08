f5677c04bdf9895603174c0be42130bfb5f92422
Sarin Na Wangkanai <sarin@wangkanai.com>
2018-02-08 21:16:17 +0700

    update license header

--- src/Wangkanai.Analytics.Tracker/ITrackerBuilder.cs
+++ src/Wangkanai.Analytics.Tracker/ITrackerBuilder.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿// Copyright (c) 2018 Sarin Na Wangkanai, All Rights Reserved.
+// The MIT License (MIT). See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
 using System;
 using System.Collections.Generic;
 using System.Linq;
