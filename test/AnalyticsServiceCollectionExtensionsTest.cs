53e1333bc8875b3c299792ee92f49d4385a50ace
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-02-09 15:51:08 +0700

    update and add back unit test project

--- test/AnalyticsServiceCollectionExtensionsTest.cs
+++ test/AnalyticsServiceCollectionExtensionsTest.cs
@@ -1,10 +1,6 @@
 ﻿﻿// Copyright (c) 2018 Sarin Na Wangkanai, All Rights Reserved.
 // The MIT License (MIT). See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
